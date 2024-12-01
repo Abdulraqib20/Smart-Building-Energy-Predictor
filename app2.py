dbc2db11a435f2978483a8d40f28960bb7f0e185
raqibcodes <abdulraqibshakir03@gmail.com>
2024-12-01 15:46:28 +0100

    add

--- app2.py
+++ app2.py
@@ -72,7 +72,7 @@ def load_and_preprocess_data(file_path):
 
 @st.cache_resource
 def load_model():
-    return tf.keras.models.load_model('second.keras')
+    return tf.keras.models.load_model('second_model.keras')
 
 # Load data and model
 try:
